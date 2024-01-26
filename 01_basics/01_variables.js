37ed2db2773b5114556a152df2c298ef3cd82cca
gauravt-cdac <142664637+gauravt-cdac@users.noreply.github.com>
2024-01-26 04:02:18 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "gaurav@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+accountEmail = "abc@js.com"
+accountPassword = "121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
